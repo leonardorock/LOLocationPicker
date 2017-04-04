14e3fde6f890dc2f11c41c6b94ca082a6e846e41
Leonardo <leonardo_rock@icloud.com>
2017-04-04 20:14:58 -0300

    Initial commit

---
+++ LOLocationPicker/LOLocationPicker.h
@@ -0,0 +1,19 @@
+//
+//  LOLocationPicker.h
+//  LOLocationPicker
+//
+//  Created by Leonardo Oliveira on 8/9/16.
+//  Copyright © 2016 Leonardo Oliveira. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for LOLocationPicker.
+FOUNDATION_EXPORT double LOLocationPickerVersionNumber;
+
+//! Project version string for LOLocationPicker.
+FOUNDATION_EXPORT const unsigned char LOLocationPickerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LOLocationPicker/PublicHeader.h>
+
+
